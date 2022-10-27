39a43e4b89456fbc18015bba9ee99b8a9018f63b
anhlt290300 <sieunhanlohung@gmail.com>
2022-10-27 23:55:09 +0700

    update login with firebase

---
+++ src/pages/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import UserTools from '../component/UserTools'
+const User = () => {
+  return (
+    <div>
+      abc
+      <Outlet />
+    </div>
+  )
+}
+
+export default User
